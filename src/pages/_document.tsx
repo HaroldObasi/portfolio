887775b4a42dc753b5754617d9fdecc9018c4895
HaroldObasi <haroldobasi2k16@gmail.com>
2023-03-18 22:23:47 +0100

    added extra poppins weight (900)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ export default function Document() {
           crossOrigin=""
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;1,400;1,600&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;0,900;1,400;1,600&display=swap"
           rel="stylesheet"
         />
       </Head>
