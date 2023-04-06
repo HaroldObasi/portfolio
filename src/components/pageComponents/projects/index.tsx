12701e59fc6be7b2ac459eb44a51072b6db9bda4
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-07 00:06:59 +0100

    responsive row spacing

--- src/components/pageComponents/projects/index.tsx
+++ src/components/pageComponents/projects/index.tsx
@@ -4,7 +4,7 @@ import ProjectCard from "./ProjectCard";
 import { Projects, ProjectItem } from "./types";
 
 const ProjectMain = ({ projects }: Projects) => (
-  <Grid container columnSpacing={2}>
+  <Grid container columnSpacing={1} rowSpacing={{ xs: 0, md: 1 }}>
     {projects.map((item: ProjectItem) => (
       <Grid item xs={12} lg={6}>
         <ProjectCard projectItem={item} />
