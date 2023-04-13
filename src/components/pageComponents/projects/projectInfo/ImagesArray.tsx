5a295fe6524ca4c405902a99ac440ac768533486
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-13 12:52:01 +0100

    created image list for images (buggy)

--- src/components/pageComponents/projects/projectInfo/ImagesArray.tsx
+++ src/components/pageComponents/projects/projectInfo/ImagesArray.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import Image from "next/image";
+import { ImageList, ImageListItem } from "@mui/material";
+import { urlFor } from "@/sanity-config";
 
 interface ImagesArrayProps {
   images: any[];
 }
 
 const ImagesArray = ({ images }: ImagesArrayProps) => {
-  return <div></div>;
+  return (
+    <ImageList sx={{ height: 450, marginTop: 2 }}>
+      {images.map((image) => (
+        <ImageListItem>
+          <img alt="Image" src={urlFor(image).url()} />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
 };
 
 export default ImagesArray;
